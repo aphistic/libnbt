42f2c97d921e6387533614b4331591754f9d4581
Erik Davidson <erik@erikd.org>
2010-01-23 12:58:42 -0600

    First Commit

---
+++ src/Tags/NbtList.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace LibNbt.Tags
+{
+    public class NbtList : NbtTag
+    {
+        public List<NbtTag> Tags { get; protected set; }
+        public NbtTagType Type { get; protected set; }
+		
+		public NbtList()
+        {
+            Name = "";
+            Tags = new List<NbtTag>();
+        }
+
+        internal override void ReadTag(Stream readStream) { ReadTag(readStream, true); }
+        internal override void ReadTag(Stream readStream, bool readName)
+        {
+            // First read the name of this tag
+            Name = "";
+            if (readName)
+            {
+                NbtString name = new NbtString();
+                name.ReadTag(readStream, false);
+
+                Name = name.Value;
+            }
+
+            NbtByte tagId = new NbtByte();
+            tagId.ReadTag(readStream, false);
+            Type = (NbtTagType)tagId.Value;
+
+            NbtInt length = new NbtInt();
+            length.ReadTag(readStream, false);
+
+            Tags.Clear();
+            for (int idx = 0; idx < length.Value; idx++)
+            {
+                switch ((NbtTagType)tagId.Value)
+                {
+                    case NbtTagType.TAG_Byte:
+                        NbtByte nextByte = new NbtByte();
+                        nextByte.ReadTag(readStream, false);
+                        Tags.Add(nextByte);
+                        break;
+                    case NbtTagType.TAG_Short:
+                        NbtShort nextShort = new NbtShort();
+                        nextShort.ReadTag(readStream, false);
+                        Tags.Add(nextShort);
+                        break;
+                    case NbtTagType.TAG_Int:
+                        NbtInt nextInt = new NbtInt();
+                        nextInt.ReadTag(readStream, false);
+                        Tags.Add(nextInt);
+                        break;
+                    case NbtTagType.TAG_Long:
+                        NbtLong nextLong = new NbtLong();
+                        nextLong.ReadTag(readStream, false);
+                        Tags.Add(nextLong);
+                        break;
+                    case NbtTagType.TAG_Float:
+                        NbtFloat nextFloat = new NbtFloat();
+                        nextFloat.ReadTag(readStream, false);
+                        Tags.Add(nextFloat);
+                        break;
+                    case NbtTagType.TAG_Double:
+                        NbtDouble nextDouble = new NbtDouble();
+                        nextDouble.ReadTag(readStream, false);
+                        Tags.Add(nextDouble);
+                        break;
+                    case NbtTagType.TAG_Byte_Array:
+                        NbtByteArray nextByteArray = new NbtByteArray();
+                        nextByteArray.ReadTag(readStream, false);
+                        Tags.Add(nextByteArray);
+                        break;
+                    case NbtTagType.TAG_String:
+                        NbtString nextString = new NbtString();
+                        nextString.ReadTag(readStream, false);
+                        Tags.Add(nextString);
+                        break;
+                    case NbtTagType.TAG_List:
+                        NbtList nextList = new NbtList();
+                        nextList.ReadTag(readStream, false);
+                        Tags.Add(nextList);
+                        break;
+                    case NbtTagType.TAG_Compound:
+                        NbtCompound nextCompound = new NbtCompound();
+                        nextCompound.ReadTag(readStream, false);
+                        Tags.Add(nextCompound);
+                        break;
+                }
+            }
+        }
+
+        internal override void WriteTag(Stream writeStream) { WriteTag(writeStream, true); }
+        internal override void WriteTag(Stream writeStream, bool writeName)
+        {
+            writeStream.WriteByte((byte)NbtTagType.TAG_List);
+            if (writeName)
+            {
+                NbtString name = new NbtString(Name);
+                name.WriteData(writeStream);
+            }
+
+            WriteData(writeStream);
+        }
+
+        internal override void WriteData(Stream writeStream)
+        {
+            NbtByte tagType = new NbtByte((byte)Type);
+            tagType.WriteData(writeStream);
+
+            NbtInt length = new NbtInt(Tags.Count);
+            length.WriteData(writeStream);
+
+            foreach (NbtTag tag in Tags)
+            {
+                tag.WriteData(writeStream);
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("TAG_List");
+            if (Name.Length > 0)
+            {
+                sb.AppendFormat("(\"{0}\")", Name);
+            }
+            sb.AppendFormat(": {0} entries\n", Tags.Count);
+
+            sb.Append("{\n");
+            foreach (NbtTag tag in Tags)
+            {
+                sb.AppendFormat("\t{0}\n", tag.ToString().Replace("\n", "\n\t"));
+            }
+            sb.Append("}");
+            return sb.ToString();
+        }
+    }
+}
