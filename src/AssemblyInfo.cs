25f8e2c165e3c4a3b8703a36fc3e85981c571a4e
Erik Davidson <erik@erikd.org>
2010-01-24 02:58:42 -0600

    Changed the version number to an actual number.

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("9253db1f-f1d4-45aa-a277-4f3ba635d651")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.1.0")]
+[assembly: AssemblyFileVersion("0.1.1.0")]
